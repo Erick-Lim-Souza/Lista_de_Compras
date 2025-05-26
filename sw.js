bba9b0a6d06e1a29f58c4fb8fdcb961ebb88c8cd
ERICK SOUZA <75149415+Erick-Lim-Souza@users.noreply.github.com>
2025-05-26 16:02:24 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'lista-compras-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-512x512.png'
+];
+
+// Instala o Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Busca recursos do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
